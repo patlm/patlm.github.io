dd80f5cbc615644e84aca06e49a83143bf2b50c0
Patrick Murphy <pmurphy1413@gmail.com>
2023-08-15 17:56:20 -0700

    Remove re-direct

--- src/routes/(other)/projects/plate-game/policy/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { browser } from '$app/environment';
-	import { goto } from '$app/navigation';
-
-	$: if (browser) {
-		goto('/projects/plate-game/policy.html');
-	}
-</script>
